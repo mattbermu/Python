60aaca78593255fac86fffc4b3c080abfc714fb9
mattbermu <mattbermu@gmail.com>
2026-01-07 14:33:22 -0500

    Refactor string operations in strings.py

--- fundamentals/strings.py
+++ fundamentals/strings.py
@@ -1,6 +1,9 @@
+# Practice working with strings in Python
+
 course = 'Python for Beginners'
 print ('Python' in course)
 print (course.replace('for', '4'))
 print (course.upper())
 print (course.lower())
+
 print (course.find('Python'))
