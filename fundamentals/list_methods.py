2994b79dbe03f4c070c66f7623f578744de3a631
mattbermu <mattbermu@gmail.com>
2026-01-07 14:34:50 -0500

    Add comments to list methods example
    Added comments to clarify the purpose of the code.

--- fundamentals/list_methods.py
+++ fundamentals/list_methods.py
@@ -1,3 +1,5 @@
+# Practice common list methods in Python
+
 numbers = [1, 2, 3, 4, 5]
 numbers.append('6')
 numbers.insert(0, 6)
@@ -5,4 +7,5 @@ numbers.remove(3)
 numbers.clear()
 print(len(numbers))
 print(1 in numbers)
+
 print(numbers)
